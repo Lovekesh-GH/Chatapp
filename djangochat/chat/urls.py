1bcba2affb89fd8b11f7592c8c52aaedae6431f7
Lovekesh <20bec022@nith.ac.in>
2021-08-07 18:15:33 +0530

    Virtual environment created and the pwa added and the bugs sorted

--- djangochat/chat/urls.py
+++ djangochat/chat/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
@@ -7,4 +8,18 @@ urlpatterns = [
     path('checkview',views.checkview,name='checkview'),
     path('send',views.send,name='send'),
     path('getMessages/<str:room>/',views.getMessages,name='getMessages'),
+    path(
+        "manifest.json",
+        TemplateView.as_view(
+            template_name="pwa/manifest.json", content_type="text/plain"
+        ),
+        name='manifest',
+    ),
+    path(
+        "serviceworker.js",
+        TemplateView.as_view(
+            template_name="pwa/serviceworker.js", content_type="text/javascript"
+        ),
+        name='serviceworker',
+    ),
 ]
